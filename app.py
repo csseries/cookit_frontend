51dfc790c96c2dfcd4dacac08fc0ae112d04b91a
Michael Weitnauer <michael.weitnauer@gmail.com>
2021-08-24 14:42:19 +0200

    display text inputs and button only if predictions were received

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from cookit_frontend.communcation import get_predictions
 
 #To Do: Add Logo
 
-st.write('Simply upload a picture of your fridge and get recipe suggestions based on what you have at home!')
+st.write('Simply upload a picture of your ingredients and get recipe suggestions based on what you have at home!')
 
 #Option to upload jpg/ png image that will be used from the model
 uploaded_file = st.file_uploader("Upload picture(s) of your fridge or pantry", type=["png", "jpg"], accept_multiple_files=False)
@@ -19,24 +19,27 @@ uploaded_file = st.file_uploader("Upload picture(s) of your fridge or pantry", t
 if uploaded_file:
     ingredients = get_predictions(uploaded_file)
 
-must_haves = st.text_input('Add ingredients that must be included (comma-separated)', 'Tomato')
-must_haves_parsed = [must for must in must_haves.split(',')]
+    if len(ingredients) > 0:
+        #For now we use a list of ingredients, later we need a list from the model that will be used for the API call
+        ingredients = st.text_input('Manually adjust ingredients (comma-separated)', ', '.join(ingredients))
+        ingredients_parsed = [ingr for ingr in ingredients.split(',')]
 
-exclusions = st.text_input("What don't you like in your food? (comma-separated)", "Coriander, Basil")
-exclusions_parsed = [excl for excl in exclusions.split(',')]
+        must_haves = st.text_input('Add ingredients that must be included (comma-separated)', 'Tomato')
+        must_haves_parsed = [must for must in must_haves.split(',')]
 
-#Add cuisine preferences with checkboxes
+        exclusions = st.text_input("What don't you like in your food? (comma-separated)", "Coriander, Basil")
+        exclusions_parsed = [excl for excl in exclusions.split(',')]
 
-# this is just to avoid making too many requests during development
-if st.button('get recipes'):
-    recipes = get_recipes(ingredients_parsed, must_haves_parsed, exclusions_parsed)
+        #Add cuisine preferences with checkboxes
 
-    if len(recipes) > 0:
-        for i in range(min(len(recipes), 10)):
-            st.image(recipes[i]['image'], recipes[i]['title'], use_column_width=False)
-            url = recipes[i]["sourceUrl"]
-            st.write("Cook this [recipe](%s) now" % url)
+        # this is just to avoid making too many requests during development
+        if st.button('get recipes'):
+            recipes = get_recipes(ingredients_parsed, must_haves_parsed, exclusions_parsed)
 
-
-    elif len(recipes) == 0:
-        st.write("Sorry, we couldn't find any recipes")
+            if len(recipes) > 0:
+                for i in range(min(len(recipes), 10)):
+                    st.image(recipes[i]['image'], recipes[i]['title'], use_column_width=False)
+                    url = recipes[i]["sourceUrl"]
+                    st.write("Cook this [recipe](%s) now" % url)
+            elif len(recipes) == 0:
+                st.write("Sorry, we couldn't find any recipes")
