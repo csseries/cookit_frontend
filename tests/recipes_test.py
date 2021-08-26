585161d1090df5dfb09f7c235df0b57b4326d61a
Lilly Kaemmerling <lilly.kaemmerling@web.de>
2021-08-26 15:46:37 +0200

    Added test to test the return of the get_recipe function

--- tests/recipes_test.py
+++ tests/recipes_test.py
@@ -1,5 +1,10 @@
 from cookit_frontend.recipes import get_recipes
 
 
-def test_get_recipes():
-    pass
+def test_return_get_recipes():
+    query = ["Pasta", "Zucchini", "Eggplant", "Tomato Sauce"]
+    ingredients = ["Basil"]
+    exclusions = ["Garlic"]
+    response = get_recipes(query, ingredients, exclusions)
+
+    assert response is not None
