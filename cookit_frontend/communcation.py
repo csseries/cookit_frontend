c801aa209041eb473bb1e9762e8d5231deed67ed
Michael Weitnauer <michael.weitnauer@gmail.com>
2021-08-26 17:12:46 +0200

    fix: don't call the prediction every time the ingredient selection changes

--- cookit_frontend/communcation.py
+++ cookit_frontend/communcation.py
@@ -1,8 +1,10 @@
 import requests
+import streamlit as st
 
 BACKEND_URL = 'https://fast-drake-318911-6gdu3ouwsa-ey.a.run.app/predict'
 
 
+@st.cache(suppress_st_warning=True)
 def get_predictions(image):
     files = {'image': image}
     response = requests.post(BACKEND_URL, files=files)
