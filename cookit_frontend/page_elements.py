be6ddc9663a08985512ebcb57bab62d1d829ca66
ClaireSopÃhie Series <clairesophie.series@gmail.com>
2021-08-26 15:14:28 +0200

    added site decorators and titles to page_elements

--- cookit_frontend/page_elements.py
+++ cookit_frontend/page_elements.py
@@ -7,6 +7,18 @@ from PIL import Image
 def page_decorators():
     return st.set_page_config(page_title='cookit', page_icon="frontend_img/favicon.png")
 
+def page_title():
+    img = Image.open("frontend_img/logo.png")
+    col1, col2, col3 = st.columns([3,3,3])
+    with col1:
+        st.write("")
+    with col2:
+        st.image(img)
+    with col3:
+        st.write("")
+    return col1, col2, col3
+
+
 def load_image(path):
     with open(path, 'rb') as f:
         data = f.read()
