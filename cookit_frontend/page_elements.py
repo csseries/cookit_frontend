69173d2b1387302e5846a33c431121f46a934097
ClaireSopÃhie Series <clairesophie.series@gmail.com>
2021-08-30 15:02:24 +0200

    added local style.css function

--- cookit_frontend/page_elements.py
+++ cookit_frontend/page_elements.py
@@ -3,6 +3,10 @@ import os
 import base64
 from PIL import Image
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
 
 def page_decorators():
     return st.set_page_config(page_title='cookit', page_icon="frontend_img/favicon.png")
